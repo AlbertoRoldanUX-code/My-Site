ca1acf224eec44c551615cea7aee33693f6f5e90
Alberto Roldán <a-f-r@hotmail.es>
2021-03-19 01:07:03 +0100

    Add contact form and remove gif

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
+"use strict";
 
-
-
-(function($) {
-  $(function() {
-    $('nav ul li a:not(:only-child)').click(function(e) {
-      $(this).siblings('.nav-dropdown').slideToggle();
-      $('.dropdown').not($(this).siblings()).hide();
+(function ($) {
+  $(function () {
+    $("nav ul li a:not(:only-child)").click(function (e) {
+      $(this).siblings(".nav-dropdown").slideToggle();
+      $(".dropdown").not($(this).siblings()).hide();
       e.stopPropagation();
     });
-    $('html').click(function() {
-      $('.nav-dropdown').hide();
+    $("html").click(function () {
+      $(".nav-dropdown").hide();
     });
-    $('#nav-toggle').click(function() {
-      $('nav ul').slideToggle();
+    $("#nav-toggle").click(function () {
+      $("nav ul").slideToggle();
     });
-    $('#nav-toggle').on('click', function() {
-      this.classList.toggle('active');
+    $("#nav-toggle").click(function () {
+      this.classList.toggle("active");
     });
   });
 })(jQuery);
-
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
