4d53753dcfaf184b3e92bf033ff1a99f8560e169
Alberto <alberto@Albertos-MacBook-Air.local>
2022-04-14 21:28:53 +0200

    Remove extra closing tag

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
 //Navbar elements:
-const menu = $('#nav-toggle');
-const dropdown = $('nav ul');
-const links = $('.nav-list');
+const menu = $("#nav-toggle");
+const dropdown = $("nav ul");
+const links = $(".nav-list");
 
 //Opens dropdown when clicking the menu
 menu.click(function () {
   dropdown.slideToggle();
-  this.classList.toggle('active');
+  this.classList.toggle("active");
 });
 
 //Closes dropdown when clicking an option
 links.click(function () {
-  menu[0].classList.toggle('active');
+  menu[0].classList.toggle("active");
   dropdown.slideUp();
 });
 
 //Makes year dynamic
-$('#year').html(new Date().getFullYear());
+$("#year").html(new Date().getFullYear());
